c3175e697ff6a0f304721c8c312028ba1b48e4f1
DontNeg <lukianmokahel123@gmail.com>
2024-04-03 19:08:11 -0700

    Added Loot Table to Vanilla Crop

--- src/main/java/dontneg/bananabaking/datagen/BananaLootTables.java
+++ src/main/java/dontneg/bananabaking/datagen/BananaLootTables.java
@@ -5,7 +5,9 @@ import dontneg.bananabaking.block.VanillaBeanCrop;
 import dontneg.bananabaking.item.BananaItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.loot.LootPool;
 import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.predicate.StatePredicate;
 
 public class BananaLootTables extends FabricBlockLootTableProvider {
@@ -20,8 +22,7 @@ public class BananaLootTables extends FabricBlockLootTableProvider {
                 .properties(StatePredicate.Builder.create()
                 .exactMatch(VanillaBeanCrop.AGE, 7));
         addDrop(BananaBlocks.VANILLA_CROP, cropDrops(
-                BananaBlocks.VANILLA_CROP,
-                BananaItems.VANILLA_BEAN, BananaItems.VANILLA_BEAN_SEEDS,
-                vanillaBeanDrops));
+                BananaBlocks.VANILLA_CROP, BananaItems.VANILLA_BEAN, BananaItems.VANILLA_BEAN_SEEDS, vanillaBeanDrops));
+
     }
 }
