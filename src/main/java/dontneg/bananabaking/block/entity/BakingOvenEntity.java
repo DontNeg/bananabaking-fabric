557d9f63c65670e6dd82a91720a04d1a769905cb
DontNeg <lukianmokahel123@gmail.com>
2024-08-07 17:09:33 -0700

    Small fix
    Recipe won't active if any of the two remainder slots are full or if the remainder from the recipe will make them go over the 64 max count limit.

--- src/main/java/dontneg/bananabaking/block/entity/BakingOvenEntity.java
+++ src/main/java/dontneg/bananabaking/block/entity/BakingOvenEntity.java
@@ -171,7 +171,9 @@ public class BakingOvenEntity extends BlockEntity implements ExtendedScreenHandl
     private boolean hasRecipe() {
         Optional<RecipeEntry<BakingRecipe>> recipe = getCurrentRecipe();
         return recipe.isPresent() && this.getStack(OUTPUT_SLOT).getCount() + recipe.get().value().getResult(null).getCount() <= 64
-                && (this.getStack(OUTPUT_SLOT).getItem() == recipe.get().value().getResult(null).getItem() || this.getStack(OUTPUT_SLOT).isEmpty());
+                && (this.getStack(OUTPUT_SLOT).getItem() == recipe.get().value().getResult(null).getItem() || this.getStack(OUTPUT_SLOT).isEmpty()) &&
+                !((((recipe.get().value().getRemainder()/10) + this.getStack(10).getCount())>this.getStack(10).getItem().getMaxCount()) ||
+                        (((recipe.get().value().getRemainder()%10) + this.getStack(11).getCount())>this.getStack(11).getItem().getMaxCount()));
     }
 
     @SuppressWarnings("DataFlowIssue")
